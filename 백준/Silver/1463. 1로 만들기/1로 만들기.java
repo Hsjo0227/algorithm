1f62dd765f05eee47eda0ef8e967cd160173e6a9
Hsjo0227 <128770861+Hsjo0227@users.noreply.github.com>
2024-09-04 16:16:03 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 15528 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,25 +1,20 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-    public static void main (String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N+1];
-        dp[1] = 0;
-        for (int i = 2; i < N + 1; i++) {
-            int result1 = -1;
-            int result2 = -1;
-            if(i%3 == 0) result1 = dp[i/3] + 1;
-            if(i%2 == 0) result2 = dp[i/2] + 1;
-            int result = dp[i-1] + 1;
-            if(result1 > 0) result = Math.min(result, result1);
-            if(result2 > 0) result = Math.min(result, result2);
-            dp[i] = result;
-        }
-        
-        System.out.println(dp[N]);
-        
-    }
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[N+1];
+		
+		for(int i = 2; i <= N; i++) {
+			dp[i] = dp[i-1]+1;
+			if(i%2 == 0) dp[i] = Math.min(dp[i/2]+1, dp[i]);
+			if(i%3 == 0) dp[i] = Math.min(dp[i/3]+1, dp[i]);
+		}
+		
+		System.out.println(dp[N]);
+	}
+
 }
