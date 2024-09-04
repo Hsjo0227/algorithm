9d6ac85206d8b6af56f85b17a292611d7dd2d03f
Hsjo0227 <128770861+Hsjo0227@users.noreply.github.com>
2024-09-04 15:36:26 +0900

    [D3] Title: 햄버거 다이어트, Time: 160 ms, Memory: 27,556 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 
@@ -14,60 +12,20 @@ public class Solution {
 			int N = Integer.parseInt(st.nextToken());
 			int L = Integer.parseInt(st.nextToken());
 			
-			int[][] ingredient = new int[N][2];
+			int[][] dp = new int[N+1][L+1];
 			
 			for(int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				ingredient[i][0] = Integer.parseInt(st.nextToken());
-				ingredient[i][1] = Integer.parseInt(st.nextToken());
-			}
-			
-			int answer = 0;
-			
-			for(int i = 1; i <= N; i++) {
-				int[] set = new int[N];
-				for(int j = N-i; j < N; j++) {
-					set[j] = 1;
-				}
+				int point = Integer.parseInt(st.nextToken());
+				int calory = Integer.parseInt(st.nextToken());
 				
-				do {
-					int calory = 0;
-					int point = 0;
-					for(int j = 0; j < N; j++) {
-						if(set[j] == 1) {
-							point += ingredient[j][0];
-							calory += ingredient[j][1];
-						}
-						if(calory > L) break;
-					}
-					if(calory > L) continue;
-					answer = Math.max(answer, point);
-				} while(np(set));
+				for(int j = 0; j <= L; j++) {
+					if(j >= calory)	dp[i+1][j] = Math.max(dp[i][j-calory] + point, dp[i][j]);
+					else dp[i+1][j] = dp[i][j];
+				}
 			}
-			
-			System.out.printf("#%d %d\n", tc, answer);
+			System.out.printf("#%d %d\n", tc, dp[N][L]);
 		}
 	}
-	
-	public static boolean np(int[] p) {
-		int N = p.length;
-		int i = N-1;
-		while(i > 0 && p[i-1] >= p[i]) --i;
-		if(i == 0) return false;
-		int j = N-1;
-		while(p[i-1] >= p[j]) --j;
-		swap(p, i-1, j);
-		
-		int k = N-1;
-		while(i < k) {
-			swap(p, i++, k--);
-		}
-		return true;
-	}
-	
-	public static void swap(int[] p, int i, int j) {
-		int temp = p[i];
-		p[i] = p[j];
-		p[j] = temp;
-	}
+
 }
