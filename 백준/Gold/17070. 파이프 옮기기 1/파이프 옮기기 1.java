8ec3e5e79a31657d1e62aa23af17993e82c76fa3
Hsjo0227 <128770861+Hsjo0227@users.noreply.github.com>
2024-09-06 17:05:19 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 64 ms, Memory: 11824 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] board = new int[N][N];
+		// 세번째는 파이프의 방향(가로, 세로, 대각선)
+		int[][][] dp = new int[N][N][3];
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		dp[0][1][0] = 1;
+		for(int i = 2; i < N; i++) {
+			if(board[0][i] != 1) dp[0][i][0] = dp[0][i-1][0];
+		}
+		
+		for(int i = 1; i < N; i++) {
+			for(int j = 1; j < N; j++) {
+				if(board[i][j] == 1) continue;
+				dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+				dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+				if(board[i-1][j] == 1 || board[i][j-1] == 1) continue;
+				dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
+			}
+		}
+		int sum = 0;
+		for(int i = 0; i < 3; i++) {
+			sum += dp[N-1][N-1][i];
+		}
+		
+		System.out.println(sum);
+	}
+
+}
