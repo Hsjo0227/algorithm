aab7802bdaac8558842ecaf4c92c2c4fbcb40576
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2024-12-08 14:28:17 +0900

    [Silver II] Title: 한 줄로 서기, Time: 64 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,34 +1,32 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] heights = new int[N];
-        Arrays.fill(heights, -1);
-
-        for(int i=1; i<N+1; i++) {
-            int number = Integer.parseInt(st.nextToken());
-            for(int j=0; j<N; j++) {
-                if(number == 0 && heights[j] == -1) {
-                    heights[j] = i;
-                    break;
-                }
-                if(heights[j] == -1) {number--;}
-            }
+        
+        int[] inputs = new int[N];
+        
+        for(int i = 0; i < N; i++) {
+        	inputs[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = N-1; i >= 0; i--) {
+        	list.add(inputs[i], i+1);
         }
 
         StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<N; i++) {
-            sb.append(heights[i]).append(" ");
+        for(int i = 0; i < N; i++) {
+            sb.append(list.get(i)).append(" ");
         }
-
+        
         System.out.println(sb);
+        
+        
     }
 }
