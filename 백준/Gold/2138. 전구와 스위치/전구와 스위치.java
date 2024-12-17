ebe7e126d97178019e5ad9d4d608d5f874fc4bbb
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2024-12-17 16:55:25 +0900

    [Gold IV] Title: 전구와 스위치, Time: 132 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
@@ -1,74 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] difference;
+	static int N;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		boolean[] arr = new boolean[N];
+		String str = br.readLine();
+		String str2 = br.readLine();
+		for(int i = 0; i < N; i++) {
+			arr[i] = (str.charAt(i) == str2.charAt(i));
+		}
+		
+		int answer = Integer.MAX_VALUE;
+		
+		// 1번 스위치 안누름
+		boolean[] copy = Arrays.copyOf(arr, N);
+		int cnt = solve(copy);
+		
+		if(cnt != -1) answer = Math.min(answer, cnt);
+		
+		// 1번 스위치 누름
+		copy = Arrays.copyOf(arr, N);
+		toggle(copy, 0);
+		cnt = solve(copy);
 
-    private static void reverse(int[] arr, int i, int num) {
-        for (int j = 0; j < num; j++) {
-            if (i + j >= N) {
-                break;
-            }
-            arr[i + j] = (arr[i + j] == 0) ? 1 : 0;
-        }
-    }
+		if(cnt != -1) answer = Math.min(answer, cnt+1);
+		
+		if(answer == Integer.MAX_VALUE) answer = -1;
+		System.out.println(answer);
+		
+		
+	}
+	public static int solve(boolean[] arr) {
+		int answer = -1;
+		
+		boolean[] goal = new boolean[N];
+		Arrays.fill(goal, true);
+		
+		int cnt = 0;
+		
+		for(int i = 1; i < N; i++) {
+			if(!arr[i-1]) {
+				toggle(arr, i);
+				cnt++;
+			}
+		}
+		
+		if(Arrays.equals(goal, arr)) {
+			answer = cnt;
+		}
+		return answer;
+		
+	}
+	
 
-    private static int search() {
-        int min = 0;
-        int count = 0;
-        int[] difference2 = Arrays.copyOf(difference, N);
-
-        for (int i = 0; i < N - 1; i++) {
-            if (difference[i] == 1) {
-                reverse(difference, i, 3);
-                count++;
-            }
-        }
-        if (difference[N - 1] == 1) min = -1;
-        else min = count;
-
-        reverse(difference2, 0, 2);
-        count = 1;
-        for (int i = 0; i < N - 1; i++) {
-            if (difference2[i] == 1) {
-                reverse(difference2, i, 3);
-                count++;
-            }
-        }
-
-        if (difference2[N - 1] != 1) {
-            if (min >= 0) {
-                min = Math.min(min, count);
-            } else {
-                min = count;
-            }
-        }
-
-        return min;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        int[] bulbs = new int[N];
-        difference = new int[N];
-        int answer = 0;
-
-        String s = br.readLine();
-        for (int i = 0; i < N; i++) {
-            bulbs[i] = Character.getNumericValue(s.charAt(i));
-        }
-
-        s = br.readLine();
-        for (int i = 0; i < N; i++) {
-            difference[i] = Math.abs(Character.getNumericValue(s.charAt(i)) - bulbs[i]);
-        }
-
-        answer = search();
-        System.out.println(answer);
-
-    }
+	public static void toggle(boolean[] arr, int i) {
+		if(i-1 >= 0) arr[i-1] = !arr[i-1];
+		arr[i] = !arr[i];
+		if(i+1 < N) arr[i+1] = !arr[i+1];
+	}
 }
