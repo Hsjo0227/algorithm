993a133c54f61f6bed58b0128406dfb12f1ff42d
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2024-12-20 22:58:04 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 92 ms, Memory: 13540 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -11,15 +11,15 @@ public class Main {
     }
     
     public static int solve(String str) {
-        StringBuilder sb = new StringBuilder(str);
-        
         int i = 1;
         
+        int end = str.length();
+        int start = 0;
         for(i = 1; i <= Integer.MAX_VALUE; i++) {
             String num = String.valueOf(i);
             for(int j = 0; j < num.length(); j++) {
-                if(sb.charAt(0) == num.charAt(j)) sb.delete(0, 1);
-                if(sb.length() == 0) return i;
+                if(str.charAt(start) == num.charAt(j)) start++;
+                if(start == end) return i;
             }
         }
         
