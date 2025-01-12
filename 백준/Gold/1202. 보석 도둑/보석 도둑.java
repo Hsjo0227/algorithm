1af67843a1e1547006d1c8b129592d4fe141279c
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-01-13 01:10:32 +0900

    [Gold II] Title: 보석 도둑, Time: 1592 ms, Memory: 134276 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,58 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-// 1202번 - 보석 도둑
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        PriorityQueue<int[]> priorityQueue = new PriorityQueue<int[]>((e1, e2) -> {
-            return e2[1] - e1[1];
-        });
-
-        int[][] jewels = new int[N][2];
-        int[] bags = new int[K];
-        long result = 0;
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            jewels[i][0] = Integer.parseInt(st.nextToken());
-            jewels[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = 0; i < K; i++) {
-            bags[i] = Integer.parseInt(br.readLine());
-        }
-
-        Arrays.sort(jewels, (j1, j2) -> {
-            return j1[0] - j2[0];
-        });
-
-        Arrays.sort(bags);
-
-        int j = 0;
-        for (int i = 0; i < K; i++) {
-            int bagCapaicty = bags[i];
-
-            while (j < N) {
-                if (bagCapaicty >= jewels[j][0]) {
-                    priorityQueue.add(jewels[j]);
-                } else {
-                    break;
-                }
-                j++;
-            }
-            if(!priorityQueue.isEmpty()){
-                result += priorityQueue.poll()[1];
-            }
-        }
-
-        System.out.println(result);
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[][] jewels = new int[N][2];
+		int[] bag = new int[K];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine()); 
+			jewels[i][0] = Integer.parseInt(st.nextToken());
+			jewels[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 0; i < K; i++) {
+			bag[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(jewels, (o1, o2) -> Integer.compare(o1[0], o2[0]));
+		Arrays.sort(bag);
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		long result = 0;
+		
+		int j = 0;
+		for(int i = 0; i < K; i++) {
+			while(j < N && jewels[j][0] <= bag[i]) {
+				pq.offer(jewels[j][1]);
+				j++;
+			}
+			
+			if(!pq.isEmpty()) {
+				result += pq.poll();
+			}
+		}
+		
+		System.out.println(result);
+	}
 }
