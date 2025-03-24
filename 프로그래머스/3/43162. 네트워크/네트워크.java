9f4e2b082098eaf9f68bd153ed584505242896d9
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-03-24 21:32:16 +0900

    [level 3] Title: 네트워크, Time: 0.55 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,37 +1,6 @@
-import java.util.*;
-
 class Solution {
-    static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visited = new boolean[n];
-        
-        for(int i = 0; i < n; i++) {
-            if(visited[i]) continue;
-            bfs(n, computers, i);
-            answer++;
-        }
-        
-        
         return answer;
     }
-    
-    public void bfs(int n, int[][] adj, int start) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(start);
-        visited[start] = true;
-        
-        while(!queue.isEmpty()) {
-            int num = queue.poll();
-            for(int i = 0; i < n; i++) {
-                if(i == num) continue;
-                if(adj[num][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    queue.offer(i);
-                } 
-            }
-            
-        }
-    }
-    
 }
