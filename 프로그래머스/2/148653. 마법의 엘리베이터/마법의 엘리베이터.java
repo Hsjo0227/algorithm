84bdd6b9ffc15276823522f1e6e395689b486ed3
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-04-30 23:40:26 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while(storey > 0) {
+            int num = storey % 10;
+            storey /= 10;
+            
+            if(num < 5) {
+                answer += num;
+            } else if(num > 5) {
+                answer += 10 - num;
+                storey++;
+            } else  {
+                answer += 5;
+                if(storey % 10 >= 5) storey++;
+            }
+        }
+        
+        return answer;
+    }
+}
