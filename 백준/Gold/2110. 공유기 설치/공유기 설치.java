ee324005cf12bc8ef5e2e83949cb15c3de1e7974
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-07-20 15:20:28 +0900

    [Gold IV] Title: 공유기 설치, Time: 252 ms, Memory: 28284 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,49 +1,56 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-import java.io.IOException;
- 
-public class Main {
+import java.io.*;
+import java.util.*;
 
-	public static void main(String args[]) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int N = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
-		long[] arr = new long[N];
-		for(int i=0; i<N; i++) {
-			arr[i] = Long.parseLong(br.readLine());
-		}
-		Arrays.sort(arr);
-		
-		long lo = 1;
-		long hi = arr[arr.length-1] - arr[0]+1;
-		long mid = 0;
-		
-		while(lo<hi) {
-			mid = (lo+hi)/2;
-			long lastHouse = arr[0];
-			int count = 1;
-			
-			for(int i=0; i<N; i++) {
-				if(arr[i]-lastHouse>=mid) {
-					count++;
-					lastHouse = arr[i];
-				}
-				
-			}//end for
-			if(count<C) {
-				hi = mid;
-			}
-			else {
-				lo = mid+1;
-			}
-			
-		}//end while
-		
-		System.out.println(lo-1);
-	}//end main
+class Main {
+    static int N, C;
+    static int[] home;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        
+        home = new int[N];
+        
+        for(int i = 0; i < N; i++) {
+            home[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(home);
+        
+        int left = 1;
+        int right = home[N-1] - home[0];
+        int answer = 0;
+        
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(check(mid)) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        System.out.println(answer);
+        
+    }
+    
+    public static boolean check(int d) {
+        int cnt = 1;
+        int prev = home[0];
+        
+        for(int i = 1; i < N; i++) {
+            int now = home[i];
+            if((now - prev) >= d) {
+                cnt++;
+                prev = now;
+                
+                if(cnt == C) return true;
+            }
+        }
+        return false;
+    }
 }
