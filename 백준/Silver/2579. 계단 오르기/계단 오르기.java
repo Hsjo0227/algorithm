0b92102bd2cbe82c46912b4a1efdd9545cdf7c51
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-09-19 09:51:10 +0900

    [Silver III] Title: 계단 오르기, Time: 64 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,23 +1,24 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         
-        int[] arr = new int[N + 1];
-        int[] dp = new int[N + 1];
-        
-        for(int i = 1; i < N + 1; i++) {
+        int[] arr = new int[N+1];
+        int[] dp = new int[N+1];
+        for(int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-        }
-        
-        dp[1] = arr[1];
-        if(N > 1) dp[2] = arr[1] + arr[2];
-        
-        for(int i = 3; i < N + 1; i++) {
-            dp[i] = Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
+            if(i > 2) {
+                dp[i] = arr[i] + Math.max(dp[i-2], dp[i-3] + arr[i-1]);
+            } else {
+                int sum = 0;
+                for(int j = i; j > 0; j--) {
+                    sum += arr[j];
+                }
+                dp[i] = sum;
+            }
         }
         
         System.out.println(dp[N]);
