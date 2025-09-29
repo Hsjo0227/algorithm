a8c3f30bdf9a3e34bc0a48a4ae7bd06667606fb0
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-09-29 23:42:04 +0900

    [Gold V] Title: 꿀 따기, Time: 208 ms, Memory: 26284 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.java
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.java
@@ -19,27 +19,22 @@ class Main {
         }
         
         long answer = 0;
-        for(int i = 1; i <= N-2; i++) {
-            for(int j = i+1; j <= N-1; j++) {
-                if(i == j) continue;
-                for(int k = j+1; k <= N; k++){
-                    if(i == k || j == k) continue;
-                    
-                    long sum = arr2[k] - arr2[i];
-                    sum += arr2[k] - arr2[j];
-                    sum -= arr[j];
-                    answer = Math.max(answer, sum);
-                    
-                    sum = arr2[j] - arr2[i];
-                    sum += arr2[k-1] - arr2[j-1];
-                    answer = Math.max(answer, sum);
-                    
-                    sum = arr2[j-1] - arr2[i-1];
-                    sum += arr2[k-1] - arr2[i-1];
-                    sum -= arr[j];
-                    answer = Math.max(answer, sum);
-                }
-            }
+        for(int i = 2; i <= N-1; i++) {
+            long sum = arr2[N] - arr2[1];
+            sum += arr2[N] - arr2[i];
+            sum -= arr[i];
+            
+            answer = Math.max(answer, sum);
+            
+            sum = arr2[i] - arr2[1];
+            sum += arr2[N-1] - arr2[i-1];
+            answer = Math.max(answer, sum);
+            
+            sum = arr2[N-1];
+            sum += arr2[i-1];
+            sum -= arr[i];
+            
+            answer = Math.max(answer, sum);
         }
         System.out.println(answer);
     }
