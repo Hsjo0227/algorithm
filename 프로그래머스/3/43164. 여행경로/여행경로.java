edcdeca642c7d3ae6dfc207aa983e06e5fc79b49
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-10-18 00:30:20 +0900

    [level 3] Title: 여행경로, Time: 0.60 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,39 +1,40 @@
 import java.util.*;
 
 class Solution {
+    static int l;
+    static String[] answer;
+    static boolean[] used;
     public String[] solution(String[][] tickets) {
-        String[] answer = new String[tickets.length+1];
-        boolean[] used = new boolean[tickets.length];
+        
+        l = tickets.length;
+        answer = new String[l+1];
+        used = new boolean[l];
         
         Arrays.sort(tickets, (arr1, arr2) -> arr1[1].compareTo(arr2[1]));
         
         answer[0] = "ICN";
-        dfs(answer, tickets, used, 0);
         
-        
+        dfs(tickets, 1);
         
         return answer;
     }
     
-    public boolean dfs(String[] answer, String[][] tickets, boolean[] used, int idx) {
-        if(idx == tickets.length) return true;
+    public boolean dfs(String[][] tickets, int idx) {
+        if(idx == l + 1) {
+            return true;
+        }
         
-        for(int i = 0; i < tickets.length; i++) {
-            String[] ticket = tickets[i];
-            
-            if(!ticket[0].equals(answer[idx])) continue;
+        for(int i = 0; i < l; i++) {
             if(used[i]) continue;
+            String[] arr = tickets[i];
             
-            answer[idx+1] = ticket[1];
+            if(!arr[0].equals(answer[idx-1])) continue;
             used[i] = true;
-            
-            if(dfs(answer, tickets, used, idx+1)) return true;
-            
-            answer[idx+1] = null;
+            answer[idx] = arr[1];
+            if(dfs(tickets, idx+1)) return true;
             used[i] = false;
+            
         }
         return false;
-        
-        
     }
 }
