56df7832710d8f9fb7d6ec4e436d066c3e183e07
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-11-28 10:12:55 +0900

    [Silver IV] Title: 듣보잡, Time: 264 ms, Memory: 28292 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        Set<String> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
+        
+        for(int i = 0; i < N; i++) {
+            set1.add(br.readLine());
+        }
+        
+        for(int i = 0; i < M; i++) {
+            set2.add(br.readLine());
+        }
+        
+        set1.retainAll(set2);
+        
+        List<String> answer = new ArrayList<>(set1);
+        Collections.sort(answer);
+        
+        sb.append(answer.size()).append('\n');
+        for(String str : answer) {
+            sb.append(str).append('\n');
+        }
+        
+        System.out.println(sb);
+    }
+}
