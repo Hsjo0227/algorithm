cb7103dc9b8e2a3a6af9746751f897df0bf9a04a
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-12-12 11:08:48 +0900

    [Silver IV] Title: 삼삼한 수, Time: 68 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/17252. 삼삼한 수/삼삼한 수.java
+++ 백준/Silver/17252. 삼삼한 수/삼삼한 수.java
@@ -5,24 +5,21 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
+
         if(N == 0) {
             System.out.println("NO");
             return;
-        } 
-        
-        long num = 1;
-        
-        while(num * 3 <= N) {
-            num *= 3;
         }
-        
-        for(; num >= 1; num = num / 3) {
-            if(num <= N) {
-                N -= num;
+
+        String str = Integer.toString(N, 3);
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == '2') {
+                System.out.println("NO");
+                return;
             }
         }
-        
-        System.out.println(N == 0 ? "YES" : "NO");
+        System.out.println("YES");
     }
 }
