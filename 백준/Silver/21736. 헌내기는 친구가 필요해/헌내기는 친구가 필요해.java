9630a4ff93e8f0a2e103d755bade64dc422786c8
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2025-12-24 12:09:06 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 264 ms, Memory: 26028 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,68 +1,56 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	static int[] parent;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		int row = Integer.parseInt(st.nextToken());
-		int col = Integer.parseInt(st.nextToken());
-		
-		Queue<int[]> que = new LinkedList<>();
-		boolean[][] visited = new boolean[row][col];
-		
-		
-		char[][] map = new char[row][col];
-		for(int i=0; i<row; i++) {
-			String str = br.readLine();
-			for(int j=0; j<col; j++) {
-				map[i][j] = str.charAt(j);
-				if(map[i][j] == 'I') {
-					que.offer(new int[] {i,j});
-					visited[i][j] = true;
-				}
-			}
-		}
-		//end input
-		
-		
-		//BFS
-		int[] nearX = new int[] {1,-1,0,0}; 
-		int[] nearY = new int[] {0,0,1,-1}; 
-		int count = 0;
-		
-		while(!que.isEmpty()) {
-			int[] location = que.poll();
-			int nowX = location[0];
-			int nowY = location[1];
-			
-			for(int i=0; i<4; i++) {
-				int nextX = nowX + nearX[i];
-				int nextY = nowY + nearY[i];
-				
-				if(nextX<0 || nextX>=row || nextY<0 || nextY>=col) {
-					continue;
-				}
-				if(map[nextX][nextY]!='X' && visited[nextX][nextY]!=true) {
-					que.offer(new int[] {nextX, nextY});
-					visited[nextX][nextY] = true;
-					if(map[nextX][nextY]=='P') {
-						count++;
-					}
-				}
-			}
-			
-		}//end while
-		
-		if(count==0) {
-			System.out.println("TT");
-		}
-		else {
-			System.out.println(count);
-		}
-		
-		
-	}
+class Main {
+    final static int[] dr = {-1, 1, 0, 0};
+    final static int[] dc = {0, 0, -1, 1};
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        char[][] board = new char[N][M];
+        
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[N][M];
+        
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < M; j++) {
+                char ch = str.charAt(j);
+                if(ch == 'I') {
+                    queue.offer(new int[] {i, j});
+                    board[i][j] = 'O';
+                } else {
+                    board[i][j] = ch;
+                }
+            }
+        }
+        
+        int cnt = 0;
+        
+        while(!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            int r = pos[0];
+            int c = pos[1];
+            
+            if(board[r][c] == 'P') cnt++;
+            for(int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                
+                if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                if(board[nr][nc] == 'X') continue;
+                if(visited[nr][nc]) continue;
+                
+                queue.offer(new int[] {nr, nc});
+                visited[nr][nc] = true;
+            }
+        }
+        
+        System.out.println(cnt == 0 ? "TT" : cnt);
+    }
 }
