f44945e9dcc0a00989af996c8b23e6fa7721e6f2
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2026-01-15 23:58:35 +0900

    [Gold V] Title: 입국심사, Time: 228 ms, Memory: 21964 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -1,46 +1,43 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main { 
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
+        long M = Long.parseLong(st.nextToken());
         
-        long left = Integer.MAX_VALUE;
-        long right = 0;
-        for(int i=0; i<N; i++) {
-        	arr[i] = Integer.parseInt(br.readLine());
-//        	right =Math.max(arr[i], right);
-        	left = Math.min(arr[i], left);
+        long[] arr = new long[N];
+        long max = 0;
+        
+        for(int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, arr[i]);
         }
-        right = left*M;
-        // end input
         
-    	while(left<=right) {
-    		long mid = left+(right-left)/2;
-//    		System.out.println(left+" "+ mid + " "+ right); 
-    		int cnt = 0;
-    		for(int i=0; i<N; i++) {	
-    			cnt+= mid/arr[i];
-    			if(cnt>=M) {
-    				break;
-    			}
+        long left = 1;
+        long right = max * M;
+        long answer = right;
+        
+        while(left <= right) {
+            long mid = (left + right) / 2;
+            
+            long cnt = 0;
+            for(int i = 0; i < N; i++) {
+                cnt += mid / arr[i];
+                if(cnt >= M) break;
             }
-    		
-//    		System.out.println(cnt);
-    		if(cnt<M) {
-    			left = mid+1;
-    		}
-    		else {
-    			right = mid-1;
-    		}
-//    		System.out.println("=================");
-    		
-    	}//ene while
-        System.out.println(left);
-       
-    }//end main
+            
+            if(cnt >= M) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        System.out.println(answer);
+    }
 }
