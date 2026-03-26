d108a9a265aa8f9fb89d87548296df77ce179dbf
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2026-03-26 09:43:30 +0900

    [level 2] Title: 배달, Time: 5.15 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,56 +1,44 @@
 import java.util.*;
 
 class Solution {
-    static int INF = Integer.MAX_VALUE / 2;
-    
     public int solution(int N, int[][] road, int K) {
-        int answer = 0;   
+        int answer = 0;
         
         List<List<int[]>> adj = new ArrayList<>();
-        
-        for (int i = 0; i <= N; i++) {
+        for(int i = 0; i <= N; i++) {
             adj.add(new ArrayList<>());
         }
         
-        for (int[] arr :  road) {
-            int start = arr[0];
-            int end = arr[1];
-            int d = arr[2];
+        for(int[] arr : road) {
+            int u = arr[0];
+            int v = arr[1];
+            int w = arr[2];
             
-            adj.get(start).add(new int[]{end, d});
-            adj.get(end).add(new int[]{start, d});
+            adj.get(u).add(new int[] {v, w});
+            adj.get(v).add(new int[] {u, w});
         }
         
-        int[] dist = new int[N+1];
-        Arrays.fill(dist, INF);
+        Queue<Integer> queue = new ArrayDeque<>();
+        int[] dist =  new int[N+1];
+        
+        queue.offer(1);
+        Arrays.fill(dist, Integer.MAX_VALUE / 2);
         dist[1] = 0;
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((arr1, arr2) -> Integer.compare(arr1[1], arr2[1]));
-        pq.offer(new int[]{1, 0});
-        
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int no = cur[0];
-            int curDist = cur[1];
+        while(!queue.isEmpty()) {
+            int no = queue.poll();
             
-            if (curDist > dist[no]) continue;
-            
-            for (int[] arr : adj.get(no)) {
-                int next = arr[0];
-                int nextDist = curDist + arr[1];
-                
-                if (nextDist < dist[next]) {
-                    dist[next] = nextDist;
-                    pq.offer(new int[] {next, nextDist});
+            for(int[] next : adj.get(no)) {
+                if(dist[next[0]] > dist[no] + next[1]) {
+                    dist[next[0]] = dist[no] + next[1];
+                    queue.offer(next[0]);
                 }
             }
-            
         }
-        
-        for (int i = 1; i <= N; i++) {
-            if (dist[i] <= K) {
-                answer++;
-            }
+        for(int i = 1; i <= N; i++) {
+            if(dist[i] == Integer.MAX_VALUE / 2) continue;
+            if(dist[i] > K) continue;
+            answer++;
         }
         
         return answer;
