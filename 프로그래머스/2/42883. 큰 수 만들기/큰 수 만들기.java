3264b18b550958c14b107271dbf99d789060fd94
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2026-04-15 09:43:16 +0900

    [level 2] Title: 큰 수 만들기, Time: 49.62 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,30 +1,38 @@
+import java.util.*;
+
 class Solution {
     public String solution(String number, int k) {
         String answer = "";
         int n = number.length();
         
         StringBuilder sb = new StringBuilder();
+        Deque<Integer> deque = new ArrayDeque<>();
+        
         
         for(int i = 0; i < n; i++) {
             int digit = number.charAt(i) - '0';
-            int l = sb.length();
+            int l = deque.size();
             if(l == 0) {
-                sb.append(digit);
+                deque.offerLast(digit);
                 continue;
             }
             
             while(l != 0 && n-i > n-k-l) {
-                int num = sb.charAt(l-1) - '0';
+                int num = deque.peekLast();
                 if(num < digit) {
-                    sb.deleteCharAt(l-1);
+                    deque.pollLast();
                     l--;
                 } else {
                     break;
                 }
             }
             if(l < n-k) {
-                sb.append(digit);
+                deque.offerLast(digit);
             }
+        }
+        
+        for(int num : deque) {
+            sb.append(num);
         }
         answer = sb.toString();
         
