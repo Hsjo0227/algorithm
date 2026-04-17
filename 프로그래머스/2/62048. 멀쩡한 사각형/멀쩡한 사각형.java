b2c6745ccafabef80320afd64b0963e675e1c059
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2026-04-17 10:30:44 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 967.91 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int w, int h) {
+        long answer = (long) w * h;
+        
+        // y = h/w * x
+        // hy = wx
+        
+        long prev = 0;
+        for(long x = 1; x <= w; x++) {
+            long y = (long)h * x / w;
+            
+            if((long)h * x % w == 0) {
+                answer -= (y - prev);
+            } else {
+                answer -= (y - prev + 1);
+            }
+            
+            prev = y;
+        }
+        
+        return answer;
+    }
+}
