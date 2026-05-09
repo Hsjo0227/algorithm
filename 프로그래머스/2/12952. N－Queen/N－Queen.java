6256d1d1c2448b716e5bdf916735176e447279fd
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2026-05-10 01:35:19 +0900

    [level 2] Title: N-Queen, Time: 178.30 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    int[] board;
+    int answer;
+    public int solution(int n) {
+        answer = 0;
+        board = new int[n];
+        Arrays.fill(board, -1);
+        
+        dfs(n, 0);
+        
+        return answer;
+    }
+    
+    private void dfs(int n, int idx) {
+        if(idx == n) answer++;
+        
+        for(int i = 0; i < n; i++) {
+            if(check(n, idx, i)) {
+                board[idx] = i;
+                dfs(n, idx+1);
+                board[idx] = -1;
+            }
+        }
+    }
+    
+    private boolean check(int n, int idx, int num) {
+        for(int i = idx - 1; i >= 0; i--) {
+            if(board[i] == num) return false;
+        }
+        
+        for(int i = -n; i <= n; i++) {
+            int row = num + i;
+            int col = idx + i;
+            if(row < 0 || row >= n || col < 0 || col >= n) continue;
+            if(board[col] == row) return false;
+        }
+        
+        for(int i = -n; i <= n; i++) {
+            int row = num + i;
+            int col = idx - i;
+            if(row < 0 || row >= n || col < 0 || col >= n) continue;
+            if(board[col] == row) return false;
+        }
+        
+        return true;
+    }
+}
