e303b9cd8d41381949560b92ec042d75d2fc0534
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2026-05-11 21:28:19 +0900

    [level 2] Title: 무인도 여행, Time: 4.26 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,46 +1,29 @@
 import java.util.*;
 
+
 class Solution {
-    static final int[] dr = {-1, 1, 0, 0};
-    static final int[] dc = {0, 0, -1, 1};
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
     
-    static int n, m;
-    static int[][] board;
-    static boolean[][] visited;
-    
-    static List<Integer> list;
-    
+    int n, m;
+    boolean[][] visited;
     public int[] solution(String[] maps) {
-        int[] answer = {};
-        
+        List<Integer> list = new ArrayList<>();
         n = maps.length;
         m = maps[0].length();
         
-        board = new int[n][m];
         visited = new boolean[n][m];
-        list = new ArrayList<>();
-        
-        for(int i = 0; i < n; i++) {
-            String str = maps[i];
-            for(int j = 0; j < m; j++) {
-                char ch = str.charAt(j);
-                if(ch == 'X') continue;
-                board[i][j] = ch - '0';
-            }
-        }
-        
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(board[i][j] == 0) continue;
+                if(maps[i].charAt(j) == 'X') continue;
                 if(visited[i][j]) continue;
-                list.add(bfs(i, j));
+                list.add(bfs(maps, i, j));
             }
         }
         
+        if(list.size() == 0) return new int[] {-1};
         Collections.sort(list);
-        if(list.isEmpty()) list.add(-1);
-        
-        answer = new int[list.size()];
+        int[] answer = new int[list.size()];
         for(int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
@@ -48,34 +31,34 @@ class Solution {
         return answer;
     }
     
-    private int bfs(int r, int c) {
+    private int bfs(String[] map, int r, int c) {
         Queue<int[]> queue = new ArrayDeque<>();
-        
         queue.offer(new int[] {r, c});
         visited[r][c] = true;
         
-        int food = 0;
+        int cnt = 0;
         
         while(!queue.isEmpty()) {
             int[] arr = queue.poll();
             r = arr[0];
             c = arr[1];
             
-            food += board[r][c];
+            cnt += map[r].charAt(c) - '0';
             
             for(int i = 0; i < 4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
                 
                 if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
-                if(board[nr][nc] == 0) continue;
+                if(map[nr].charAt(nc) == 'X') continue;
                 if(visited[nr][nc]) continue;
                 
                 queue.offer(new int[] {nr, nc});
                 visited[nr][nc] = true;
             }
+            
         }
-        
-        return food;
+        return cnt;
     }
+    
 }
