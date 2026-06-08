1b7bb6b181dbd0ef29debbe212a07f0476f00a0d
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2026-06-08 21:51:26 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.49 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,82 +1,84 @@
 import java.util.*;
 
 class Solution {
-    final static int[] dr = {-1,1,0,0};
-    final static int[] dc = {0,0,-1,1};
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
     
-    static int[][] map;
-    static int R, C;
+    static int n, m;
+
     public int solution(String[] board) {
         int answer = 0;
-        R = board.length;
-        C = board[0].length();
-        
-        map = new int[R][C];
-        int r = 0;
-        int c = 0;
-        
-        for(int i = 0; i < R; i++) {
-            String str = board[i];
-            for(int j = 0; j < C; j++) {
-                char ch = str.charAt(j);
-                if(ch == 'D') {
-                    map[i][j] = 1;
-                } else if(ch == 'G') {
-                    map[i][j] = 2;
-                } else if(ch == 'R') {
-                    r = i;
-                    c = j;
+
+        n = board.length;
+        m = board[0].length();
+
+        int[] start = new int[2];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i].charAt(j) == 'R') {
+                    start[0] = i;
+                    start[1] = j;
                 }
             }
         }
-        
+
         Queue<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[R][C];
-        
-        queue.offer(new int[] {r, c});
-        visited[r][c] = true;
-        
+        boolean[][] visited = new boolean[n][m];
+
+        queue.offer(start);
+        visited[start[0]][start[1]] = true;
+
         int depth = 0;
-        while(!queue.isEmpty()) {
+
+        while (!queue.isEmpty()) {
             int size = queue.size();
-            while(size-- > 0) {
-                int[] pos = queue.poll();
-                r = pos[0];
-                c = pos[1];
-                
-                if(map[r][c] == 2) return depth;
-                
-                for(int i = 0; i < 4; i++) {
-                    int[] next = move(r, c, i);
+
+            while (size-- > 0) {
+                int[] cur = queue.poll();
+                int r = cur[0];
+                int c = cur[1];
+
+                if (board[r].charAt(c) == 'G') {
+                    return depth;
+                }
+
+                for (int dir = 0; dir < 4; dir++) {
+                    int[] next = move(board, r, c, dir);
+
                     int nr = next[0];
                     int nc = next[1];
-                    
-                    if(visited[nr][nc]) continue;
-                    queue.offer(new int[] {nr, nc});
+
+                    if (visited[nr][nc]) {
+                        continue;
+                    }
+
                     visited[nr][nc] = true;
+                    queue.offer(next);
                 }
             }
+
             depth++;
         }
-        
-        
+
         return -1;
     }
-    
-    private int[] move(int r, int c, int dir) {
-        while(true) {
+
+    public int[] move(String[] board, int r, int c, int dir) {
+        while (true) {
             int nr = r + dr[dir];
             int nc = c + dc[dir];
-            
-            if(nr < 0 || nr >= R || nc < 0 || nc >= C) {
+
+            if (nr < 0 || nr >= n || nc < 0 || nc >= m) {
                 return new int[] {r, c};
             }
-            if(map[nr][nc] == 1) {
+
+            if (board[nr].charAt(nc) == 'D') {
                 return new int[] {r, c};
             }
+
             r = nr;
             c = nc;
         }
     }
-    
 }
