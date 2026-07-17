8a1a73e3cfa0ab3ada9080265529d89118a68037
조현석 <128770861+Hsjo0227@users.noreply.github.com>
2026-07-17 14:06:04 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 30.93 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,37 +1,40 @@
 import java.util.*;
 
 class Solution {
-    static int[] answer;
-    static Map<String, Integer> map;
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        answer = new int[enroll.length];
-        map = new HashMap<>();
+        int n = enroll.length;
+        int m = seller.length;
+        int[] answer = new int[n];
         
-        for(int i = 0; i < enroll.length; i++) {
-            map.put(enroll[i], i);
+        Map<String, String> map = new HashMap<>();
+        Map<String, Integer> idxs = new HashMap<>();
+        
+        for(int i = 0; i < n; i++) {
+            String child = enroll[i];
+            String parent = referral[i];
+            
+            idxs.put(child, i);
+            
+            if(parent.equals("-")) continue;
+            
+            map.put(child, parent);
         }
         
-        for(int i = 0; i < seller.length; i++) {
-            int people = map.get(seller[i]);
-            int money = amount[i] * 100;
-            calc(enroll, referral, people, money);
+        for(int i = 0; i < m; i++) {
+            String sellerName = seller[i];
+            int remain = amount[i] * 100;
+            
+            do {
+                int idx = idxs.get(sellerName);
+                
+                answer[idx] += remain - (remain / 10);
+                remain = remain / 10;
+                
+                sellerName = map.get(sellerName);
+                
+            } while(sellerName != null && remain != 0);
         }
         
         return answer;
-    }
-    
-    public static void calc(String[] enroll, String[] referral, int idx, int money) {
-        if(money == 0) return;
-        if(referral[idx].equals("-")) {
-            answer[idx] += (int) Math.ceil(money * 0.9);
-            return;
-        }
-        
-        answer[idx] += (int) Math.ceil(money * 0.9);
-        int ref = map.get(referral[idx]);
-        
-        calc(enroll, referral, ref, (int) Math.floor(money * 0.1));
-        
-        
     }
 }
